0905eeee23386889f58b1e4980d331dd7b26d23d
Alexey Gulev <me@agulev.com>
2020-06-14 12:13:07 +0200

    Defold 1.2.170 fixes

--- jstodef/lib/web/lib_jstodef.js
+++ jstodef/lib/web/lib_jstodef.js
@@ -17,25 +17,25 @@ var LibJsToDef = {
                     var msg_id = allocate(intArrayFromString(message_id), "i8", ALLOC_NORMAL);
                     switch (typeof message) {
                         case 'undefined':
-                            Runtime.dynCall("vi", JsToDef._callback_empty, [msg_id]);
+                            dynCall("vi", JsToDef._callback_empty, [msg_id]);
                             break;
                         case 'number':
-                            Runtime.dynCall("vif", JsToDef._callback_number, [msg_id, message]);
+                            dynCall("vif", JsToDef._callback_number, [msg_id, message]);
                             break;
                         case 'string':
                             var msg = allocate(intArrayFromString(message), "i8", ALLOC_NORMAL);
-                            Runtime.dynCall("vii", JsToDef._callback_string, [msg_id, msg]);
+                            dynCall("vii", JsToDef._callback_string, [msg_id, msg]);
                             Module._free(msg);
                             break;
                         case 'object':
                             var msg = JSON.stringify(message);
                             msg = allocate(intArrayFromString(msg), "i8", ALLOC_NORMAL);
-                            Runtime.dynCall("vii", JsToDef._callback_object, [msg_id, msg]);
+                            dynCall("vii", JsToDef._callback_object, [msg_id, msg]);
                             Module._free(msg);
                             break;
                         case 'boolean':
                             var msg = message ? 1 : 0; 
-                            Runtime.dynCall("vii", JsToDef._callback_bool, [msg_id, msg]);
+                            dynCall("vii", JsToDef._callback_bool, [msg_id, msg]);
                             break;
                         default:
                             console.warn("Unsupported message format: " + (typeof message));
